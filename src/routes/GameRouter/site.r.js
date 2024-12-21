3171c2659d5c18fb99cad5736f21d37afb041ee6
Phuong1809 <21120534@student.hcmus.edu.vn>
2024-12-22 01:51:24 +0700

    update profile page

--- src/routes/GameRouter/site.r.js
+++ src/routes/GameRouter/site.r.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.use('*',AuthCheck);
 router.get('/gamepage',SiteController.GetGamePage);
 router.get('/match',SiteController.GetMatchPage);
+router.get('/profile',SiteController.GetProfilePage);
 
 
 
